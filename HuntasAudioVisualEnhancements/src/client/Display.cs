2df0e4d393896f45dfa51c80cd169e893da9cd7d
HuntaBadday <huntabadday@gmail.com>
2026-05-20 20:00:09 -0300

    Makes standing displays work properly

--- HuntasAudioVisualEnhancements/src/client/Display.cs
+++ HuntasAudioVisualEnhancements/src/client/Display.cs
@@ -104,14 +104,14 @@ public class PanelDisplay : LogicWorld.ClientCode.PanelDisplay {
 }
 
 public class StandingDisplay : LogicWorld.ClientCode.StandingDisplay {
-    /*private static readonly SoundEffect lightOn = SoundEffectDatabase.GetSoundEffectByTextID("HuntasAudioVisualEnhancements.lightOn");
+    private static readonly SoundEffect lightOn = SoundEffectDatabase.GetSoundEffectByTextID("HuntasAudioVisualEnhancements.lightOn");
     private static readonly SoundEffect lightOff = SoundEffectDatabase.GetSoundEffectByTextID("HuntasAudioVisualEnhancements.lightOff");
     
     private float currentR = 0.0f;
     private float currentG = 0.0f;
     private float currentB = 0.0f;
     
-    private bool lastState = false;
+    private bool[] lastStates;
     
     private CoilBuzz buzz;
     
@@ -119,58 +119,68 @@ public class StandingDisplay : LogicWorld.ClientCode.StandingDisplay {
     
     protected override void Initialize() {
         base.Initialize();
-        buzz = new CoilBuzz(this);
+        //buzz = new CoilBuzz(this);
         
-        Type typeGenericDisplay = typeof(GenericDisplay<IPanelDisplayData>);
+        Type typeGenericDisplay = typeof(GenericDisplay<IDisplayData>);
         GetCurrentColor = Methods.getPrivate(typeGenericDisplay, "<FrameUpdate>g__GetCurrentColor|2_0");
+        
+        lastStates = new bool[InputCount];
     }
     
     protected override void FrameUpdate() {
         base.FrameUpdate();
-        bool state = GetInputState(0);
         
         //Logger.Info(ModClass.RampOnSpeed.ToString());
         
-        float displayIntensity = ModClass.DisplayIntensity;
+        if (ModClass.Enabled) {
+            float displayIntensity = ModClass.DisplayIntensity;
+            
+            GpuColor blockColor = (GpuColor)GetCurrentColor.Invoke(this, null);
+            float blockR = blockColor.r;
+            float blockG = blockColor.g;
+            float blockB = blockColor.b;
+            
+            currentR = calcNewValue(currentR, blockR);
+            currentG = calcNewValue(currentG, blockG);
+            currentB = calcNewValue(currentB, blockB);
+            if (currentR != blockR || currentG != blockG || currentB != blockB) {
+                ContinueUpdatingForAnotherFrame();
+            }
+            
+            SetBlockColor(new GpuColor(currentR*displayIntensity, currentG*displayIntensity, currentB*displayIntensity));
+        }
         
-        GpuColor blockColor = (GpuColor)GetCurrentColor.Invoke(this, null);
-        float blockR = blockColor.r;
-        float blockG = blockColor.g;
-        float blockB = blockColor.b;
-        
-        currentR = calcNewValue(currentR, blockR);
-        currentG = calcNewValue(currentG, blockG);
-        currentB = calcNewValue(currentB, blockB);
-        if (currentR != blockR || currentG != blockG || currentB != blockB) {
+        if (lastStates.Length != InputCount) {
+            lastStates = new bool[InputCount];
             ContinueUpdatingForAnotherFrame();
         }
         
-        SetBlockColor(new GpuColor(currentR*displayIntensity, currentG*displayIntensity, currentB*displayIntensity));
-        
-        if (state && !lastState) {
-            buzz.Play();
-            SoundPlayer.PlaySoundAt(lightOn, Address);
-        } else if (!state && lastState) {
-            buzz.Stop();
-            SoundPlayer.PlaySoundAt(lightOff, Address);
+        if (ModClass.SoundEnabled) {
+            for (int i = 0; i < InputCount; i++) {
+                bool state = GetInputState(i);
+                if (state && !lastStates[i]) {
+                    SoundPlayer.PlaySoundAt(lightOn, Address);
+                } else if (!state && lastStates[i]) {
+                    SoundPlayer.PlaySoundAt(lightOff, Address);
+                }
+                lastStates[i] = state;
+            }
         }
-        
-        lastState = state;
     }
     
     protected override void OnComponentDestroyed() {
         base.OnComponentDestroyed();
-        buzz.Stop();
+        //buzz.Stop();
     }
     
     public void RestartBuzz() {
-        buzz.Stop();
-        buzz.Play();
+        //buzz.Stop();
+        //buzz.Play();
     }
     
     private static float calcNewValue(float current, float setpoint) {
-        if (Math.Abs(setpoint - current) < 0.005f) {
-            return Math.Clamp(setpoint, 0.0f, 1.0f);
+        if (Math.Abs(setpoint - current) < 0.01f) {
+            return setpoint;
         }
         
         if (current > setpoint) {
@@ -178,5 +188,5 @@ public class StandingDisplay : LogicWorld.ClientCode.StandingDisplay {
         } else {
             return Math.Clamp(current + (setpoint - current) * ModClass.RampOnSpeed/100.0f, 0.0f, 1.0f);
         }
-    }*/
+    }
 }
